dce5db401c6fb98b279f1cd04f06b328a5d8246a
ZhLi7 <60142425+ZhLi7@users.noreply.github.com>
2024-04-03 12:43:50 -0400

    HOME2 mod
    updated codes to include insulin and glucose to calculate HOME2

--- preprocess for ML.R
+++ preprocess for ML.R
@@ -9,47 +9,53 @@ library(tidyr)
 ## systolic and diastolic blood pressure, and TGL:HDL ratio)
 
 jhs<-readRDS(paste0(path_endotypes_folder,"/working/cleaned/jhs.RDS")) %>% 
-  dplyr::mutate(ratio_th=tgl/hdlc)%>% 
-  dplyr::select(study_id,age,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration)
+  dplyr::mutate(ratio_th=tgl/hdlc,
+                glucosef2=glucosef*0.0555,
+                insulinf2=insulinf*6)%>% 
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration,glucosef2,insulinf2)
 
 jhs_newdm <- jhs[jhs$dmduration%in% c(0, 1), ] #some duplicates, reason unknown 
+
+summary(jhs_newdm)
 
 
 la<-readRDS(paste0(path_endotypes_folder,"/working/cleaned/look_ahead.RDS")) %>% 
   dplyr::mutate(ratio_th=tgl/hdlc)%>% 
-  dplyr::select(study_id,bsage,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration)%>% 
-  rename(age=bsage)
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration) #no fasting insulin
 
 la_newdm <- la[la$dmduration%in% c(0, 1), ] #N=877
 
 accord<-readRDS(paste0(path_endotypes_folder,"/working/cleaned/accord.RDS")) %>% 
-  dplyr::mutate(ratio_th=tgl/hdlc,bmi = weight/((height/100)^2))%>% 
-  dplyr::select(study_id,bsage,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration)%>% 
-  rename(age=bsage)
+  dplyr::mutate(ratio_th=tgl/hdlc,
+                bmi = weight/((height/100)^2)
+                )%>% 
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,dmduration) # no fasting insulin
+
 
 accord_newdm <-accord[accord$dmduration%in% c(0, 1), ] #N=601 
 
 dpp<-readRDS(paste0(path_endotypes_folder,"/working/cleaned/dpp.RDS"))%>% 
-  dplyr::mutate(ratio_th=tgl/hdlc)%>% 
-  dplyr::select(study_id,bsage,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag)%>% 
-  rename(age=bsage) #n=802 
+  dplyr::mutate(ratio_th=tgl/hdlc,
+                glucosef2=glucosef*0.0555,
+                insulinf2=insulinf*6)%>% 
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,glucosef2,insulinf2) #n=802 
 
 dppos<-readRDS(paste0(path_endotypes_folder,"/working/cleaned/dppos.RDS"))%>% 
-  dplyr::mutate(ratio_th=tgl/hdlc)%>% 
-  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag) # n = 907
+  dplyr::mutate(ratio_th=tgl/hdlc,
+                glucosef2=glucosef*0.0555,
+                insulinf2=insulinf*6
+                )%>% 
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,glucosef2,insulinf2) # n = 907
 
-view(dppos)
 
 #merge data and remove all NAs, imputation could be used in sensitivity analysis to increase sample size
 data<-bind_rows(jhs_newdm,la_newdm,accord_newdm,dpp,dppos)%>% 
-  dplyr::select(-dmduration,-study_id,-age)%>% 
-  drop_na() 
-
+  dplyr::select(study_id,bmi,hba1c,ldlc,hdlc,tgl,sbp,dbp,ratio_th,dmagediag,glucosef2,insulinf2)%>% 
+  drop_na() #1637 [LA and ACCORD excluded due to missing insulin for the HOMA2 calculation]
 
 # export to pyhton 
 data_array <- as.matrix(data)
 write.csv(data_array, paste0(path_endotypes_folder,"/working/processed/data_array.csv"), row.names = FALSE)
-
-
+# the HOMA2 is calculated using the excel calculator released by University of Oxford. two observations were removed due to extreme values out of the range. 
 
 
